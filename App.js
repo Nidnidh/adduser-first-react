4837379450563959e4e8cedf963b9cc6497b661b
Nidnidh <136248787+Nidnidh@users.noreply.github.com>
2024-01-23 19:23:24 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AddUser from "./AddUser/user/component";
+
+function App(){
+    return {
+        <div>
+        <AddUser />
+        </div>
+    };
+}
+
+export default App;
